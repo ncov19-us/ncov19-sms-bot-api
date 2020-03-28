8b0387bdbc147ecc6635ec6b8faf95c07ebb9952
ElijahMcKay <elijahmckay10@gmail.com>
2020-03-28 01:38:48 -0500

    fix: removed redundant "sms" from post route in sms-route.js

--- routes/sms-routes.js
+++ routes/sms-routes.js
@@ -1,7 +1,7 @@
 // enabling easy use of environment variables through a .env file
 require('dotenv').config(); // THIS MUST COME BEFORE TWILIO DECLARATION
 // using env variables for this shorthand verification
-const client = require("twilio")(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+// const client = require("twilio")(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN); may not be necessary for the time being
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 // one liner to instantiate Express Router
@@ -13,6 +13,30 @@ const router = require('express').Router();
 // ======== Routes =========
 
 // -- POST Routes --
+router.post('/', (req, res) => {
+    // twilio webhooks expects their TwiMl SML format specified here: https://www.twilio.com/docs/glossary/what-is-twilio-markup-language-twiml
+    // can also use raw XML in lieu of provided helper functions
+    const twiml = new MessagingResponse();
+
+    // setting inital message, request header contents
+    twiml.message("The robots are coming!");
+    res.writeHead(200, { 'Content-Type': "text/xml" });
+    res.end(twiml.toString());
+
+    // // method for sending messages
+    // client.messages
+    // .create({
+    //     body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+    //     from: "+17735707220",
+    //     to: "+17408416256"
+    // })
+    // .then(message => {
+    //     console.log(message.sid);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
+})
 
 
 module.exports = router;
