b1d843cee7bf43af165e81d3fd6fc8f3d3e11532
ElijahMcKay <elijahmckay10@gmail.com>
2020-03-29 03:15:02 -0500

    fix: fixed PORT var to work in production

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = require("./app.js");
 // enabling easy use of environment variables through a .env file
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+
 // test endpoint to determine status of API
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API working!" });
 });
 
 // app listens on the decided port of the team/environment
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}...`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
 
 module.exports = app;
