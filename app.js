2a5b04a4af2e7038cb778ab7bf1a225a983ae19d
azrap <azraap@gmail.com>
2020-04-02 15:07:33 -0700

    fix:added express.json() to enable req.body intake

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // library imports
 const express = require("express");
 // enabling easy use of environment variables through a .env file
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const cors = require("cors"); // easily manage cors access
@@ -13,6 +13,7 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: true })); // learn more about this middleware here: https://expressjs.com/en/5x/api.html#express.urlencoded
 app.use(helmet());
+app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 // route imports
