e9cdd26c4b95fd5a3a33dc9b82b035b234759703
ElijahMcKay <elijahmckay10@gmail.com>
2020-04-03 20:14:01 -0500

    refactor: added new state average logic to web route

--- routes/sms-routes.js
+++ routes/sms-routes.js
@@ -62,7 +62,7 @@ const countiesPerState = {
   CT: 8,
   RI: 5,
   HI: 5,
-  DE: 3
+  DE: 3,
 };
 // ======== Routes =========
 
@@ -74,97 +74,6 @@ router.post("/web", (req, res) => {
   const postalCode = req.body.zip;
   const phonenumber = req.body.phonenumber.replace(/[,.-]/g, "");
 
-  console.log(phonenumber);
-
-  let county, state;
-
-  client.messages
-    .create({
-      body: messageBody,
-      from: "+18133950040",
-      to: `+1${phonenumber}`
-    })
-    .then(message => console.log("test", message))
-    .catch(err => console.log(err));
-
-  axios
-    .get(
-      `https://api.opencagedata.com/geocode/v1/google-v3-json?q=countrycode=us|postcode=${postalCode}&key=${process.env.GEOCODING_KEY}&limit=1`
-    )
-    .then(res => {
-      const formatedAddressArray = res.data.results[0].formatted_address.split(
-        ","
-      );
-      // console.log("FORMATED ADDRESS ARRAY", formatedAddressArray);
-      state = formatedAddressArray[1].split(" ")[1];
-      const countyArray = formatedAddressArray[0].split(" ");
-      countyArray.pop();
-      county = countyArray.join(" ");
-      // console.log(county);
-      // declaring options for POST request to main API
-      const postOptions = {
-        state: state,
-        county: county
-      };
-
-      console.log("post options", postOptions);
-
-      let stateInfo = {};
-      axios
-        .post(`${process.env.DASHBOARD_API_URL}/county`, postOptions)
-        .then(res => {
-          countyInfo = { ...res.data.message[0] };
-          console.log(countyInfo);
-          // ${postOptions.state}
-          // ${postOptions.county} County
-          // Confirmed cases today: ${stateInfo.todays_confirmed}
-          // Total confirmed cases: ${stateInfo.confirmed}
-          // Tested today: ${stateInfo.todays_tested}
-          // Total tested: ${stateInfo.tested}
-          // Today's deaths: ${stateInfo.todays_deaths}
-          // Total deaths: ${stateInfo.deaths}
-          // res.writeHead(200, { "Content-Type": "text/xml" });
-          // res.end(twiml.toString());
-
-          client.messages
-            .create({
-              body: "Success 🤮",
-              from: "+18133950040",
-              to: `${phonenumber}`
-            })
-            .then(message => console.log("test", message))
-            .catch(err => console.log(err));
-        })
-        .catch(err => {
-          console.log(err);
-          client.messages
-            .create({
-              body: "There was a problem on our end",
-              from: "+18133950040",
-              to: `${phonenumber}`
-            })
-            .then(message => console.log("test", message))
-            .catch(err => console.log(err));
-        });
-    })
-    .catch(err => {
-      // setting inital message in case of failure, request header contents
-      client.messages
-        .create({
-          body: "Please use a 5 digit zip code.",
-          from: "+18133950040",
-          to: `${req.body.phonenumber}`
-        })
-        .then(message => console.log("test", message))
-        .catch(err => console.log(err));
-    });
-});
-
-// endpoint for users who prompt app via SMS
-router.post("/", (req, res) => {
-  const postalCode = req.body.Body;
-  // console.log("req.body", req.body);
-
   // instantiating county and state vars
   let county, state;
 
@@ -172,7 +81,7 @@ router.post("/", (req, res) => {
     .get(
       `https://api.opencagedata.com/geocode/v1/google-v3-json?q=countrycode=us|postcode=${postalCode}&key=${process.env.GEOCODING_KEY}&limit=1`
     )
-    .then(res => {
+    .then((res) => {
       const formatedAddressArray = res.data.results[0].formatted_address.split(
         ","
       );
@@ -186,7 +95,7 @@ router.post("/", (req, res) => {
       // declaring options for POST request to main API
       const postOptions = {
         state: state,
-        county: county
+        county: county,
       };
 
       // console.log("post options", postOptions);
@@ -194,14 +103,156 @@ router.post("/", (req, res) => {
 
       axios
         .post(`${process.env.DASHBOARD_API_URL}/county`, postOptions)
-        .then(res => {
+        .then((res) => {
           console.log(res.data.message);
           countyInfo = { ...res.data.message[0] };
 
           // post request to build comparisons to state averages to send to user
           axios
             .post(`${process.env.DASHBOARD_API_URL}/stats`, { state: state })
-            .then(res => {
+            .then((res) => {
+              let numOfCounties = countiesPerState[state];
+              console.log(res.data.message);
+              // let [ tested, todays_tested, confirmed, todays_confirmed, deaths, todays_deaths ] = res.data.message;
+              console.log(res.data.message.todays_confirmed);
+              let newCaseIncrease =
+                countyInfo.new /
+                (res.data.message.todays_confirmed / numOfCounties);
+              let totalCaseIncrease =
+                countyInfo.confirmed /
+                (res.data.message.confirmed / numOfCounties);
+              let newDeathIncrease =
+                countyInfo.new_death /
+                (res.data.message.todays_deaths / numOfCounties);
+              let totalDeathIncrease =
+                countyInfo.death / (res.data.message.deaths / numOfCounties);
+
+              console.log("new", newCaseIncrease);
+              // let fatalityRateIncrease = countyInfo.fatality_rate / s
+
+              function upOrDown(num) {
+                let arrow;
+
+                if (num > 0) {
+                  arrow = "\u2B06";
+                  console.log("up");
+                  return arrow;
+                } else if (num === 0) {
+                  arrow = "";
+                  console.log("none");
+                  return arrow;
+                } else {
+                  arrow = "\u2B07";
+                  console.log("down");
+                  return arrow;
+                }
+              }
+
+              const countyMessageBody = `
+  ${countyInfo.county_name} County, ${countyInfo.state_name}
+
+  Cases Today: ${countyInfo.new} (${upOrDown(
+                newCaseIncrease
+              )} ${newCaseIncrease.toFixed(2)}% from state avg.)
+  Total Cases: ${countyInfo.confirmed} (${upOrDown(
+                totalCaseIncrease
+              )} ${totalCaseIncrease.toFixed(2)}% from state avg.)
+  Deaths Today: ${countyInfo.new_death} (${upOrDown(
+                newDeathIncrease
+              )} ${newDeathIncrease.toFixed(2)}% from state avg.)
+  Total Deaths: ${countyInfo.death} (${upOrDown(
+                totalCaseIncrease
+              )} ${totalCaseIncrease.toFixed(2)}% from state avg.)
+  Fatality Rate: ${countyInfo.fatality_rate}
+            `;
+
+              // console.log(stateArr);
+              client.messages
+                .create({
+                  body: countyMessageBody,
+                  from: "+18133950040",
+                  to: `${phonenumber}`,
+                })
+                .then((message) => console.log("test", message))
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+          client.messages
+            .create({
+              body: "There was a problem on our end.  Please try again later",
+              from: "+18133950040",
+              to: `${phonenumber}`,
+            })
+            .then((message) => console.log("test", message))
+            .catch((err) => console.log(err));
+        });
+    })
+    .catch((err) => {
+      // setting inital message in case of failure, request header contents
+      client.messages
+        .create({
+          body: "Please use a 5 digit zip code.",
+          from: "+18133950040",
+          to: `${phonenumber}`,
+        })
+        .then((message) => console.log("test", message))
+        .catch((err) => console.log(err));
+    });
+});
+
+
+
+
+
+
+
+// endpoint for users who prompt app via SMS
+router.post("/", (req, res) => {
+  const postalCode = req.body.Body;
+  // console.log("req.body", req.body);
+
+  // instantiating county and state vars
+  let county, state;
+
+  axios
+    .get(
+      `https://api.opencagedata.com/geocode/v1/google-v3-json?q=countrycode=us|postcode=${postalCode}&key=${process.env.GEOCODING_KEY}&limit=1`
+    )
+    .then((res) => {
+      const formatedAddressArray = res.data.results[0].formatted_address.split(
+        ","
+      );
+      // console.log("FORMATED ADDRESS ARRAY", formatedAddressArray);
+      state = formatedAddressArray[1].split(" ")[1];
+      // console.log(state)
+      const countyArray = formatedAddressArray[0].split(" ");
+      countyArray.pop();
+      county = countyArray.join(" ");
+      // console.log(county);
+      // declaring options for POST request to main API
+      const postOptions = {
+        state: state,
+        county: county,
+      };
+
+      // console.log("post options", postOptions);
+      let countyInfo, stateInfo;
+
+      axios
+        .post(`${process.env.DASHBOARD_API_URL}/county`, postOptions)
+        .then((res) => {
+          console.log(res.data.message);
+          countyInfo = { ...res.data.message[0] };
+
+          // post request to build comparisons to state averages to send to user
+          axios
+            .post(`${process.env.DASHBOARD_API_URL}/stats`, { state: state })
+            .then((res) => {
               let numOfCounties = countiesPerState[state];
               console.log(res.data.message);
               // let [ tested, todays_tested, confirmed, todays_confirmed, deaths, todays_deaths ] = res.data.message;
@@ -262,12 +313,12 @@ Fatality Rate: ${countyInfo.fatality_rate}
                 .create({
                   body: countyMessageBody,
                   from: "+18133950040",
-                  to: `${req.body.From}`
+                  to: `${req.body.From}`,
                 })
-                .then(message => console.log("test", message))
-                .catch(err => console.log(err));
+                .then((message) => console.log("test", message))
+                .catch((err) => console.log(err));
             })
-            .catch(err => {
+            .catch((err) => {
               console.log(err);
             });
 
@@ -282,28 +333,28 @@ Fatality Rate: ${countyInfo.fatality_rate}
           // Total deaths: ${stateInfo.deaths}
           // `
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
           client.messages
             .create({
               body: "There was a problem on us",
               from: "+18133950040",
-              to: `${req.body.From}`
+              to: `${req.body.From}`,
             })
-            .then(message => console.log("test", message))
-            .catch(err => console.log(err));
+            .then((message) => console.log("test", message))
+            .catch((err) => console.log(err));
         });
     })
-    .catch(err => {
+    .catch((err) => {
       // setting inital message in case of failure, request header contents
       client.messages
         .create({
           body: "Please use a 5 digit zip code.",
           from: "+18133950040",
-          to: `${req.body.From}`
+          to: `${req.body.From}`,
         })
-        .then(message => console.log("test", message))
-        .catch(err => console.log(err));
+        .then((message) => console.log("test", message))
+        .catch((err) => console.log(err));
     });
 });
 
